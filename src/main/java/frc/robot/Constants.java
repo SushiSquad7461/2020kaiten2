74940e7fc746a4fd3869cd4af6f4dcadcfc975e8
Aryan Jha <aryan@jha.io>
2020-01-25 02:17:19 -0800

    add open loop drive constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,38 +12,47 @@ public final class Constants {
 	// drivetrain
 	public static final class Drivetrain {
 
+		// motors
+		public static final int FL_ID = 5;
+		public static final int FR_ID = 1;
+		public static final int BR_ID = 4;
+		public static final int BL_ID = 8;
+
+		// configuration
+		public static final int	CURRENT_LIMIT = 40;
+
 	}
 
 	// flywheel
 	public static final class Flywheel {
 
-        // motors
-        public static final int MAIN_ID = 2;
-        public static final int SECONDARY_ID = 3;
+		// motors
+		public static final int MAIN_ID = 2;
+		public static final int SECONDARY_ID = 3;
 
-        // encoders
-        public static final int ENCODER_A = 1234;
-        public static final int ENCODER_B = 4321;
-        public static final boolean reverseEncoder = false;
+		// encoders
+		public static final int ENCODER_A = 1234;
+		public static final int ENCODER_B = 4321;
+		public static final boolean reverseEncoder = false;
 
-        // flywheel speed (rotations per minute)
-        public static final double SPEED = 60;
+		// flywheel speed (rotations per minute)
+		public static final double SPEED = 60;
 
-        // don't tune these--documentation stated constants for entering rpm
-        public static final int TICKS_PER_ROTATION = 4096; // ticks per one encoder rotation
-        public static final double SETPOINT_CONSTANT = 0.001667; // 100 ms / 1 min
+		// don't tune these--documentation stated constants for entering rpm
+		public static final int TICKS_PER_ROTATION = 4096; // ticks per one encoder rotation
+		public static final double SETPOINT_CONSTANT = 0.001667; // 100 ms / 1 min
 
-        // pid constants
-        public static final double kP = 0;
-        public static final double kI = 0;
-        public static final double kD = 0;
-        public static final double kS = 0;
-        public static final double kV = 0;
-        public static final double kA = 0;
-        public static final double ERROR_TOLERANCE = 0;
+		// pid constants
+		public static final double kP = 0;
+		public static final double kI = 0;
+		public static final double kD = 0;
+		public static final double kS = 0;
+		public static final double kV = 0;
+		public static final double kA = 0;
+		public static final double ERROR_TOLERANCE = 0;
 
-        // timeout value for parameter configs
-        public static final int CONFIG_TIMEOUT = 30;
+		// timeout value for parameter configs
+		public static final int CONFIG_TIMEOUT = 30;
 
 	}
 
