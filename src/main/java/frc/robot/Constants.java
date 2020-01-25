0e32c1f9be36da00d673b583a75b49cae71e565a
Aryan Jha <aryan@jha.io>
2020-01-25 02:07:36 -0800

    add flywheel constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -9,21 +9,49 @@ package frc.robot;
 
 public final class Constants {
 
-    // drivetrain
-    public static final class Drivetrain {
+	// drivetrain
+	public static final class Drivetrain {
 
-    }
+	}
 
-    // flywheel
-    public static final class Flywheel {
+	// flywheel
+	public static final class Flywheel {
 
-    }
+        // motors
+        public static final int MAIN_ID = 2;
+        public static final int SECONDARY_ID = 3;
 
-    // camera
-    public static final class Camera {
+        // encoders
+        public static final int ENCODER_A = 1234;
+        public static final int ENCODER_B = 4321;
+        public static final boolean reverseEncoder = false;
 
-    }
+        // flywheel speed (rotations per minute)
+        public static final double SPEED = 60;
 
-    // controllers
+        // don't tune these--documentation stated constants for entering rpm
+        public static final int TICKS_PER_ROTATION = 4096; // ticks per one encoder rotation
+        public static final double SETPOINT_CONSTANT = 0.001667; // 100 ms / 1 min
+
+        // pid constants
+        public static final double kP = 0;
+        public static final double kI = 0;
+        public static final double kD = 0;
+        public static final double kS = 0;
+        public static final double kV = 0;
+        public static final double kA = 0;
+        public static final double ERROR_TOLERANCE = 0;
+
+        // timeout value for parameter configs
+        public static final int CONFIG_TIMEOUT = 30;
+
+	}
+
+	// camera
+	public static final class Camera {
+
+	}
+
+	// controllers
 
 }
