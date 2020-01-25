90cc59a57450630a0bbe23eecba5c1526528b159
Aryan Jha <aryan@jha.io>
2020-01-25 01:46:20 -0800

    configure constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,13 +7,23 @@
 
 package frc.robot;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants.  This class should not be used for any other purpose.  All constants should be
- * declared globally (i.e. public static).  Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
 public final class Constants {
+
+    // drivetrain
+    public static final class Drivetrain {
+
+    }
+
+    // flywheel
+    public static final class Flywheel {
+
+    }
+
+    // camera
+    public static final class Camera {
+
+    }
+
+    // controllers
+
 }
