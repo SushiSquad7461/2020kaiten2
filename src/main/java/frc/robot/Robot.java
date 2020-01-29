e6d7201fd4a3b23a63fe2f6603a661de1bde59b0
Everett Wang <everettwang@outlook.com>
2020-01-28 20:07:26 -0800

    use shuffleboard to select the auto path

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,19 +7,29 @@
 
 package frc.robot;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
   public Paths paths;
   private RobotContainer m_robotContainer;
+  SendableChooser<SequentialCommandGroup> autoChooser = new SendableChooser<>();
 
   @Override
   public void robotInit() {
     m_robotContainer = new RobotContainer();
     paths = new Paths();
+    autoChooser.setDefaultOption("Example path", RamseteCommands.ExamplePath.fullPath());
+    autoChooser.addOption("Offensive 1", RamseteCommands.ExamplePath.fullPath());
+    SmartDashboard.putData("Auto path", autoChooser);
   }
 
   @Override
@@ -38,7 +48,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    m_autonomousCommand = autoChooser.getSelected();
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
     }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,11 @@
 
 package frc.robot;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.*;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
@@ -14,7 +19,7 @@ import frc.robot.subsystems.ExampleSubsystem;
 public class RobotContainer {
 	// define subsystems and commands
 	private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-	public static final Drivetrain s_drive = new Drivetrain();
+	static Drivetrain s_drive = new Drivetrain();
 
 	private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
