a1f8b8612683dbb03f11d7e4b7f2d3cb11859b5a
Everett Wang <everettwang@outlook.com>
2020-02-07 18:30:19 -0800

    fixed tab formatting

--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -18,73 +18,73 @@ import frc.robot.Constants.ClimbConstants;
 
 public class Climb extends ProfiledPIDSubsystem {
 
-  // initialize the motors
-  private final WPI_TalonSRX deployTalon;
-  private final WPI_TalonSRX winchTalon;
-  private final WPI_VictorSPX winchVictor;
-  private final CANCoder climbArmEncoder;
-  private final ElevatorFeedforward climbArmFeedForward;
+	// initialize the motors
+	private final WPI_TalonSRX deployTalon;
+	private final WPI_TalonSRX winchTalon;
+	private final WPI_VictorSPX winchVictor;
+	private final CANCoder climbArmEncoder;
+	private final ElevatorFeedforward climbArmFeedForward;
 
-  // constructor
-  public Climb()  {
-    super(new ProfiledPIDController(ClimbConstants.ARM_kP, ClimbConstants.ARM_kI, ClimbConstants.ARM_kD,
-            new TrapezoidProfile.Constraints(ClimbConstants.MAX_VELOCITY_RAD_PER_SEC, ClimbConstants.MAX_ACCEL)), ClimbConstants.BASE_POSE);
-    // define the motor controllers
-    deployTalon = new WPI_TalonSRX(ClimbConstants.DEPLOY_TALON);
-    winchTalon = new WPI_TalonSRX(ClimbConstants.WINCH_TALON);
-    winchVictor = new WPI_VictorSPX(ClimbConstants.WINCH_VICTOR);
+	// constructor
+	public Climb()  {
+		super(new ProfiledPIDController(ClimbConstants.ARM_kP, ClimbConstants.ARM_kI, ClimbConstants.ARM_kD,
+				new TrapezoidProfile.Constraints(ClimbConstants.MAX_VELOCITY_RAD_PER_SEC, ClimbConstants.MAX_ACCEL)), ClimbConstants.BASE_POSE);
+		// define the motor controllers
+		deployTalon = new WPI_TalonSRX(ClimbConstants.DEPLOY_TALON);
+		winchTalon = new WPI_TalonSRX(ClimbConstants.WINCH_TALON);
+		winchVictor = new WPI_VictorSPX(ClimbConstants.WINCH_VICTOR);
 
-    // set victors to follow talonsrx
-    winchVictor.follow(winchTalon);
+		// set victors to follow talonsrx
+		winchVictor.follow(winchTalon);
 
-    // initialize encoder
-    climbArmEncoder = new CANCoder(ClimbConstants.CLIMB_CAN_ID);
+		// initialize encoder
+		climbArmEncoder = new CANCoder(ClimbConstants.CLIMB_CAN_ID);
 
-    // initialize feedforward
-    climbArmFeedForward = new ElevatorFeedforward(ClimbConstants.kS, ClimbConstants.kV, ClimbConstants.kA);
-  }
+		// initialize feedforward
+		climbArmFeedForward = new ElevatorFeedforward(ClimbConstants.kS, ClimbConstants.kV, ClimbConstants.kA);
+	}
 
-  public void calculateInput(double input) {
-    double output = (input/ClimbConstants.CLIMB_ELEVATOR_DISTANCE_PER_ROTATION);
-    goToSetpoint(output);
-  }
+	public void calculateInput(double input) {
+		double output = (input/ClimbConstants.CLIMB_ELEVATOR_DISTANCE_PER_ROTATION);
+		goToSetpoint(output);
+	}
 
-  // theoretically the way to apply the ProfiledPIDController
-  public void goToSetpoint(double goalPose) {
-    double calculateChange = m_controller.calculate(climbArmEncoder.getAbsolutePosition(), goalPose);
-    double climbFF = climbArmFeedForward.calculate(m_controller.getSetpoint().position,
-            m_controller.getSetpoint().velocity);
-    deployTalon.setVoltage(calculateChange + climbFF);
-  }
+	// theoretically the way to apply the ProfiledPIDController
+	public void goToSetpoint(double goalPose) {
+		double calculateChange = m_controller.calculate(climbArmEncoder.getAbsolutePosition(), goalPose);
+		double climbFF = climbArmFeedForward.calculate(m_controller.getSetpoint().position,
+				m_controller.getSetpoint().velocity);
+		deployTalon.setVoltage(calculateChange + climbFF);
+	}
 
-  @Override
-  public void useOutput(double output, TrapezoidProfile.State setpoint) { }
+	@Override
+	public void useOutput(double output, TrapezoidProfile.State setpoint) { }
 
-  @Override
-  public double getMeasurement() {
-    return climbArmEncoder.getAbsolutePosition() + ClimbConstants.BASE_POSE;
-  }
+	@Override
+	public double getMeasurement() {
+		return climbArmEncoder.getAbsolutePosition() + ClimbConstants.BASE_POSE;
+	}
 
-  public void dropElevator() { goToSetpoint(ClimbConstants.BASE_POSE); }
+	public void dropElevator() { goToSetpoint(ClimbConstants.BASE_POSE); }
 
-  public void startDeployClimbArm() {
-    deployTalon.set(ClimbConstants.DEPLOY_SPEED);
-  }
+	public void startDeployClimbArm() {
+		deployTalon.set(ClimbConstants.DEPLOY_SPEED);
+	}
 
-  public void stopDeployClimbArm() {
-    deployTalon.set(0);
-  }
+	public void stopDeployClimbArm() {
+		deployTalon.set(0);
+	}
 
-  public void startWinch() {
-    winchTalon.set(ClimbConstants.WINCH_SPEED);
-  }
+	public void startWinch() {
+		winchTalon.set(ClimbConstants.WINCH_SPEED);
+	}
 
-  public void stopWinch() {
-    winchTalon.set(0);
-  }
+	public void stopWinch() {
+		winchTalon.set(0);
+	}
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+	@Override
+	public void periodic() {
+		// This method will be called once per scheduler run
+	}
 }
